4a52099f47cdabb1d5b4b2e1418dca24b06d91d9
dimasta00 <58218506+dimasta00@users.noreply.github.com>
2020-04-12 01:58:03 +0300

    test
    test

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="1; url=https://n.apclick.ru/click/5e9238cd2bfa8102af2d30b5/120079/248243/mywebsite">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>  
